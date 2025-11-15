b738178113e2392421696cf6d531fd7deb4c9b8c
Genie 2.0 <lp72311432579@gmail.com>
2025-11-15 14:59:27 -0800

    Add mana costs to food spells
    Updated foodSpells to include mana costs for spells.

--- wwfoodspells.js
+++ wwfoodspells.js
@@ -6,30 +6,27 @@ const LOW = 1;
 const MEDIUM = 2;
 const HIGH = 3;
 
-class Spell {
-  constructor(power, level) {
-    this.power = power;
-    this.level = level;
-  }
-}
+const MANA_LOW = 10 * LOW;
+const MANA_MEDIUM = 10 * MEDIUM;
+const MANA_HIGH = 10 * HIGH;
 
 const foodSpells = {
-  "Nacho nuke": new Spell(getRandomInt(5, 8), LOW),
-  "Sugarshock": new Spell(getRandomInt(5, 8), LOW),
-  "Spicebomb": new Spell(getRandomInt(5, 8), LOW),
-  "Frosting fix": new Spell(getRandomInt(5, 8), LOW),  // heal
-  "Gumdrop glow": new Spell(getRandomInt(5, 8), LOW),  // heal
-  "Wasabi whiplash": new Spell(getRandomInt(5, 8), MEDIUM),
-  "Gravy grenade": new Spell(getRandomInt(5, 8), MEDIUM),
-  "Fries of fury": new Spell(getRandomInt(5, 8), MEDIUM),
-  "Rhubarb remedy": new Spell(getRandomInt(5, 8), MEDIUM), // heal
-  "Casserole cure": new Spell(getRandomInt(5, 8), MEDIUM), // heal
-  "Beetroot boost": new Spell(getRandomInt(5, 8), MEDIUM), // heal
-  "Exploding cabbage curse": new Spell(getRandomInt(5, 8), HIGH),
-  "Banana peel barrage": new Spell(getRandomInt(5, 8), HIGH),
-  "Molten marshmallow mayhem": new Spell(getRandomInt(5, 8), HIGH),
-  "Ghost pepper blast": new Spell(getRandomInt(5, 8), HIGH),
-  "Vanilla vibe check": new Spell(getRandomInt(5, 8), HIGH),       // heal
-  "Gravy fueled recovery": new Spell(getRandomInt(5, 8), HIGH),    // heal
-  "Avocado aura boost": new Spell(getRandomInt(5, 8), HIGH)        // heal
+  "Nacho nuke": new Spell(getRandomInt(5, 8), LOW, MANA_LOW),
+  "Sugarshock": new Spell(getRandomInt(5, 8), LOW, MANA_LOW),
+  "Spicebomb": new Spell(getRandomInt(5, 8), LOW, MANA_LOW),
+  "Frosting fix": new Spell(getRandomInt(5, 8), LOW, MANA_LOW),  // heal
+  "Gumdrop glow": new Spell(getRandomInt(5, 8), LOW, MANA_LOW),  // heal
+  "Wasabi whiplash": new Spell(getRandomInt(5, 8), MEDIUM, MANA_MEDIUM),
+  "Gravy grenade": new Spell(getRandomInt(5, 8), MEDIUM, MANA_MEDIUM),
+  "Fries of fury": new Spell(getRandomInt(5, 8), MEDIUM, MANA_MEDIUM),
+  "Rhubarb remedy": new Spell(getRandomInt(5, 8), MEDIUM, MANA_MEDIUM), // heal
+  "Casserole cure": new Spell(getRandomInt(5, 8), MEDIUM, MANA_MEDIUM), // heal
+  "Beetroot boost": new Spell(getRandomInt(5, 8), MEDIUM, MANA_MEDIUM), // heal
+  "Exploding cabbage curse": new Spell(getRandomInt(5, 8), HIGH, MANA_HIGH),
+  "Banana peel barrage": new Spell(getRandomInt(5, 8), HIGH, MANA_HIGH),
+  "Molten marshmallow mayhem": new Spell(getRandomInt(5, 8), HIGH, MANA_HIGH),
+  "Ghost pepper blast": new Spell(getRandomInt(5, 8), HIGH, MANA_HIGH),
+  "Vanilla vibe check": new Spell(getRandomInt(5, 8), HIGH, MANA_HIGH),       // heal
+  "Gravy fueled recovery": new Spell(getRandomInt(5, 8), HIGH, MANA_HIGH),    // heal
+  "Avocado aura boost": new Spell(getRandomInt(5, 8), HIGH, MANA_HIGH)        // heal
 };
