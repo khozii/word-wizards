657a5ed5d9432faa78437af71ea36318a43ce923
Lilly <lp72311432579@gmail.com>
2025-11-15 16:24:50 -0800

    Trying to add the mana once again

--- public/wwclasses.js
+++ public/wwclasses.js
@@ -1,6 +1,7 @@
 export class Player1 {
     constructor() {
         this.health = 100;
+        this.mana = 50
         this.shield = 0;
     }
 }
@@ -8,14 +9,16 @@ export class Player1 {
 export class Player2 {
     constructor() {
         this.health = 100;
+        this.mana = 50
         this.shield = 0;
     }
 }
 
 export class Spell {
-    constructor(name, damage, tier) {
+    constructor(name, damage, tier, mana) {
         this.name = name;
         this.damage = damage;
         this.tier = tier;
+        this.mana = mana;
     }
 }
